9f6d0cc46683ab33f9a69b75e075ff7c729bbd9a
JoelAbuga <jlabuga2k16@gmail.com>
2024-07-10 10:02:50 +0800

    updated accounts

--- src/App.js
+++ src/App.js
@@ -9,11 +9,11 @@ import UgcGallery from "./components/UgcGallery";
 
 function App() {
   //Instance ID
-  let InstanceId_Widget = "520240";
-  let InstanceId_QA = "787167";
+  let InstanceId_Widget = "797457";
+  let InstanceId_QA = "";
   let InstanceId_SEO = "";
   let InstanceId_Carousel = "";
-  let InstanceId_StarRating = "520241";
+  let InstanceId_StarRating = "797458";
   let InstanceId_PromotedProd = "";
   let InstanceId_UgcGallery = "";
   let InstanceId_ReviewsTab = "";
