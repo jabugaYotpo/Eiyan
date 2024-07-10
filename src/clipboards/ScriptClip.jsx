1de06edbc85f822a12dbf1dd122eff72efc1e45a
JoelAbuga <jlabuga2k16@gmail.com>
2024-07-10 10:40:16 +0800

    update accnt

--- src/clipboards/ScriptClip.jsx
+++ src/clipboards/ScriptClip.jsx
@@ -3,7 +3,7 @@ import { CopyToClipboard } from "react-copy-to-clipboard";
 
 function ScriptClipBoard() {
   const [value, setValue] = useState(`
-  <script src="https://cdn-widgetsrepository.yotpo.com/v1/loader/NxUmhAwuUbCSbKfz0Z5VHdL5JBmmEAEZB66AQ7Ql" async></script>
+  <script src="https://cdn-widgetsrepository.yotpo.com/v1/loader/ZQm0wDkc4mCR4VL4GHInh8viqtai48TEP3VLzuYD" async></script>
     `);
   const [isCopied, setCopied] = useState(false);
 
